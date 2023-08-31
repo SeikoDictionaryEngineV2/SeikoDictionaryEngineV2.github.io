7831082e53e61477b394e20d34c98921365899a1
kagg886 <iveour@163.com>
2023-08-31 19:17:46 +0800

    fix: 🐛 修复上下文

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const nav = require("./nav.js");
 
 module.exports = {
     title: "Seiko词库引擎",
+    base: '/dist/',
     description: "语法简洁，函数易用的跨平台词库引擎",
     markdown: {
         lineNumbers: true,
